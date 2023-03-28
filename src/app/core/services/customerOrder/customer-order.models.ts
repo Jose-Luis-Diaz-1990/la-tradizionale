2e7e6d0c27b097c47b16fff2bda6cb1fa588aae2
jonatan85 <114008152+jonatan85@users.noreply.github.com>
2023-03-28 22:59:49 +0200

    feat(jonatan)Orden de Compra

---
+++ src/app/core/services/customerOrder/customer-order.models.ts
@@ -0,0 +1,10 @@
+export interface CustomerOrder {
+    _id: string,
+    order: string,
+    name: string,
+    surname: string,
+    email: string,
+    phoneNumber: number,
+    shippingAddress: string,
+    store: string,
+}
